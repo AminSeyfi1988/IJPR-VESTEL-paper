dabd4f16d08f7eff79d2bd909700e3e3fad64982
AminSeyfi1988 <121482980+AminSeyfi1988@users.noreply.github.com>
2025-08-28 18:31:02 +0300

    Batch_Algorithm_Large.py

--- Batch_Algorithm_Large.py
+++ Batch_Algorithm_Large.py
@@ -5,11 +5,11 @@ import random
 
 
 ucc = 300000
-period = (4, 6, 24,48,72)   
+period = (4,6,24,48,72)   
 instances = (0.25, 0.35, 0.45)
 seed = (14,3,15,13,7,1,16) 
 workers = (16,20)
-Invent1 = (4,7,8)
+Invent1 = (2,3,4,6,8)
 
 
 # Invent1 = (1.5,3,4.5,11)
@@ -37,5 +37,6 @@ dfres = pd.DataFrame(MyFinalObjectives)
 with pd.ExcelWriter('C:/Amin/University/Ph.d/TUBITAK 2244/Project/Final Results Algorithm/Results_Algorithm.xlsx') as writer:
      dfres.to_excel(writer, sheet_name='General information', index=False)
  
+
 
 
